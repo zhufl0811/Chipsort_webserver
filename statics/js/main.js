11f9e9fc89cf16aafa3c861a13b22b362abd2047
zhufl0811 <zhufengliang0811@163.com>
2018-06-13 10:38:32 +0800

    Add navbar

--- statics/js/main.js
+++ statics/js/main.js
@@ -18,3 +18,44 @@ function dopost(URL, PARAMS) {
     }
 }
 
+var url_to_name=new Array()
+url_to_name['']='首页';
+url_to_name['attendance']='考勤首页';
+url_to_name['admin']='后台管理';
+url_to_name['workday']='工作日设定';
+url_to_name['workarrangement']='排班';
+url_to_name['adduser']='添加用户';
+url_to_name['deluser']='删除用户';
+url_to_name['edituser']='修改用户';
+url_to_name['addrole']='添加角色';
+url_to_name['delrole']='删除角色';
+url_to_name['editrole']='修改角色';
+url_to_name['addper']='添加权限';
+url_to_name['editper']='修改权限';
+url_to_name['delper']='删除权限';
+
+function navbar() {
+    var navdiv = document.getElementById('navbar');
+    var url_splited=(window.location.pathname).split('/');
+    if(url_splited[1]==''){
+        var index_bar=document.createElement('div');
+        index_bar.innerHTML="<a href='/' />>首页";
+        navdiv.appendChild(index_bar);
+    }
+    else{
+        for(var i=0;i<url_splited.length;i++){
+            if(i==0){
+                var href_url='/'
+            }
+            else{
+                var href_url='';
+                for(var j=1;j<=i;j++){
+                    href_url+=('/'+url_splited[j])
+                }
+            }
+            var sub_bar = document.createElement('div');
+            sub_bar.innerHTML='<a href='+href_url+' />>'+url_to_name[url_splited[i]];
+            navdiv.appendChild(sub_bar)
+        }
+    }
+}
